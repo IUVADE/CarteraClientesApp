c7724bc37c396a5b135a7ede846232e5d0405b3e
TheoRev <hrevillafernandez79@gmail.com>
2017-12-01 17:59:44 -0500

    Correccion del layout filtroClienteLayout.xml

--- app/src/main/java/com/iuvade/iuvade3/carteraclientesapp/FiltroClienteActivity.java
+++ app/src/main/java/com/iuvade/iuvade3/carteraclientesapp/FiltroClienteActivity.java
@@ -1,13 +1,29 @@
 package com.iuvade.iuvade3.carteraclientesapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FiltroClienteActivity extends AppCompatActivity {
+
+    private Button btnUbicar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtro_cliente);
+
+        btnUbicar = findViewById(R.id.btnUbicar);
+
+        btnUbicar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intentMaps = new Intent(FiltroClienteActivity.this, MapsActivity.class);
+                startActivity(intentMaps);
+            }
+        });
     }
 }
